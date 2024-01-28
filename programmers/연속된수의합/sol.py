00b16964f9a1f12604a9dfe2b99559bc9449479b
AlishaKang <oneoneone920920920@gmail.com>
2024-01-28 16:32:50 +0900

    update

--- programmers/연속된수의합/sol.py
+++ programmers/연속된수의합/sol.py
@@ -1,2 +1,12 @@
 def solution(num, total):
-    
+    list = []
+    result =[]
+    for i in range(num):
+        list.append(i)
+    a = sum(list)
+    n = (total-a)//num 
+    for i in range(num):
+        result.append(i+n)
+    return result
+
+print(solution(3, 12))
