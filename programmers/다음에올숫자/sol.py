f8f0a875f7863438919f3462fcff84225d1cf586
AlishaKang <oneoneone920920920@gmail.com>
2024-01-29 21:01:13 +0900

    update

--- programmers/다음에올숫자/sol.py
+++ programmers/다음에올숫자/sol.py
@@ -1,10 +1,18 @@
+# def solution(common):
+#     a = common[1] - common[0]
+#     b = common[1] // common[0]
+#     if common[2] == common[1] + a:
+#         result = common[-1] + a
+#     else:
+#         result = common[-1] * b
+#     return result
+
 def solution(common):
-    a = common[1] - common[0]
-    b = common[1] // common[0]
-    if common[2] == common[1] + a:
-        result = common[-1] + a
+    # 등차수열인 경우
+    if common[1] - common[0] == common[-1] - common[-2]:
+        return common[-1] + (common[1] - common[0])
+    # 등비수열인 경우
     else:
-        result = common[-1] * b
-    return result
+        return common[-1] * (common[-1] // common[-2])
 
 print(solution([1, 2, 3, 4]))
