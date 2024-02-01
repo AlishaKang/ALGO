2ffbdf0ee7ebe2bb18f8b7510d6fab69f0898e0c
AlishaKang <oneoneone920920920@gmail.com>
2024-02-01 09:39:45 +0900

    update

---
+++ programmers/LV1문제/문자열내p와y의개수/sol.py
@@ -0,0 +1,7 @@
+def solution(s):
+    if s.count('p') + s.count('P') == s.count('y') + s.count('Y'):
+        return True
+    elif s.count('p') + s.count('P') == 0 and s.count('y') + s.count('Y')== 0:
+        return True
+    else:    
+        return False 
