7949947e03622ae48d6712d6ec218ffca79cb35e
AlishaKang <oneoneone920920920@gmail.com>
2024-02-19 15:37:07 +0900

    update

--- programmers/LV1문제/3진법뒤집기/sol.py
+++ programmers/LV1문제/3진법뒤집기/sol.py
@@ -1,6 +1,8 @@
 def solution(n):
     answer = ''
     while n > 0:
-        n,r = divmod(n,3)
+        n,r = divmod(n,3) # 몫과 나마지
         answer += str(r)
-    return int(answer, 3)
+    return int(answer, 3) # 3진법 기반을 10진법으로 변환
+
+print(solution(45))
