c83d77bd61617d28926ca107beffefa49fceb341
AlishaKang <oneoneone920920920@gmail.com>
2024-02-27 23:32:59 +0900

    update

--- programmers/LV1문제/문자열내마음대로정렬하기/sol.py
+++ programmers/LV1문제/문자열내마음대로정렬하기/sol.py
@@ -1,1 +1,3 @@
-solution = lambda strings, n : sorted(strings, key = lambda x:x[n]+x)
+def solution(strings, n):
+    return sorted(strings, key=lambda x:(x[n],x))
+# sorted(array, key = lambda x : x[0]) 이런식으로 key를 사용해 정렬 가능
