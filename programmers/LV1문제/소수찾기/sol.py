43357ed0457c033d190beed244c546b7384cca75
AlishaKang <oneoneone920920920@gmail.com>
2024-12-07 17:42:55 +0900

    update

--- programmers/LV1문제/소수찾기/sol.py
+++ programmers/LV1문제/소수찾기/sol.py
@@ -1,8 +1,33 @@
+# def solution(n):
+#     answer = 0
+#     for i in range(2, n):
+#         if n % i == 0:
+#             pass
+#         else:
+#             answer += 1 
+#     return answer
+
+# def solution(n):
+#     answer = [] # 확인용
+#     for i in range(2, n):
+#         if n % i == 0:
+#             pass
+#         else:
+#             answer.append(n) 
+#     return answer
+
+import math
+def isPrime(n):
+    for i in range(2, int(math.sqrt(n))+1):
+        if n % i == 0:
+            return False
+    return True
+
 def solution(n):
     answer = 0
-    for i in range(2, n):
-        if n % i == 0:
-            pass
-        else:
-            answer += 1 
+    for i in range(2, n+1):
+        if isPrime(i) == True:
+            answer += 1
     return answer
+
+print(solution(10))
